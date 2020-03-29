095651821db20653140039839bf81cbf2a4fb059
Erika Miwa <erikamiwa612@gmail.com>
2020-03-28 23:57:34 -0700

    solved TwoSum.js

--- src/TwoSum.js
+++ src/TwoSum.js
@@ -12,7 +12,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for(let i = 0, i < length.nums, i++) {
-      for(let j = 0, j < length.n)
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = i + 1; j < nums.length; j++) {
+      if(nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
   }
 };
