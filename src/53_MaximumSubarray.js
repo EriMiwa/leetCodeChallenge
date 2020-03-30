86bff3ae746881a124bb277cf441f657c29c5b98
Erika Miwa <erikamiwa612@gmail.com>
2020-03-30 13:09:37 -0700

    solved Maximum Subarray

---
+++ src/53_MaximumSubarray.js
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/maximum-subarray/
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// Example:
+
+// Input: [-2,1,-3,4,-1,2,1,-5,4],
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let maxSum=nums[0];
+let currentSum=maxSum;
+
+for(let i=1; i<nums.length; i++){
+  currentSum=Math.max(nums[i]+currentSum, nums[i]);
+  maxSum=Math.max(currentSum, maxSum);
+}
+return maxSum;
+};
